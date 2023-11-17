9d0256094c48d7bc097b2f498ba9f6cc9d0c28bb
kevintejdiaz <kevintejdiaz@gmail.com>
2023-11-17 16:37:14 +0100

    Esta es la primera version del programa

---
+++ ProyectoC#/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    {
+        int numero1, numero2, total;
+        Console.WriteLine("Introduce el primer numero: ");
+        numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Introduce el segundo numero: ");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+        total = numero1 + numero2;
+        Console.WriteLine("La suma de numero1 y numero2 es: " + total);
+    }
+    }
+}
