622499e2c92aa206c3e0bea719500c10b72cd46c
kevintejdiaz <kevintejdiaz@gmail.com>
2023-11-17 16:42:10 +0100

    Esta es la segunda version del programa y hemos cambiado la operacion de suma a resta

--- ProyectoC#/Program.cs
+++ ProyectoC#/Program.cs
@@ -8,8 +8,8 @@
         numero1 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Introduce el segundo numero: ");
         numero2 = Convert.ToInt32(Console.ReadLine());
-        total = numero1 + numero2;
-        Console.WriteLine("La suma de numero1 y numero2 es: " + total);
+        total = numero1 - numero2;
+        Console.WriteLine("La resta de numero1 y numero2 es: " + total);
     }
     }
 }
